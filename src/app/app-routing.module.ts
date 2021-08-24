5e2d6b6e211802aaa491ccffb0a88bb2634ac346
Caitano <josiasmartinscaitano@gmail.com>
2021-08-24 16:09:44 -0300

    começando o projeto de angular, do angular.io

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component:HeroDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+            
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
